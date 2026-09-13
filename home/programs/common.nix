bb8434904c9e8fe4ac103f3ec8bf5dae92b36619
Yu <echo.aimless@gmail.com>
2026-09-13 21:15:22 +0800

    chore: setup jq + reformat nix files

--- home/programs/common.nix
+++ home/programs/common.nix
@@ -1,47 +1,45 @@
 { palette, ... }:
 
 {
-  programs.eza = {
-    enable = true;
-    enableFishIntegration = true;
-    icons = "never";
-  };
+  programs = {
+    fd.enable = true;
+    ripgrep.enable = true;
+    jq.enable = true;
 
-  programs.zoxide = {
-    enable = true;
-    enableFishIntegration = true;
-  };
-
-  programs.bat = {
-    enable = true;
-    config.theme = palette.name;
-    themes.${palette.name}.src = palette.replaceVars ../templates/bat.tmTheme.plist;
-  };
-
-  programs.btop = {
-    enable = true;
-    settings = {
-      color_theme = palette.name;
-      theme_background = true;
+    eza = {
+      enable = true;
+      enableFishIntegration = true;
+      icons = "never";
     };
-    themes.${palette.name} = palette.replaceVars ../templates/btop.theme;
-  };
 
-  programs.difftastic = {
-    enable = true;
-    git.enable = true;
-  };
+    zoxide = {
+      enable = true;
+      enableFishIntegration = true;
+    };
 
-  programs.fd = {
-    enable = true;
-  };
+    bat = {
+      enable = true;
+      config.theme = palette.name;
+      themes.${palette.name}.src = palette.replaceVars ../templates/bat.tmTheme.plist;
+    };
 
-  programs.ripgrep = {
-    enable = true;
-  };
+    btop = {
+      enable = true;
+      settings = {
+        color_theme = palette.name;
+        theme_background = true;
+      };
+      themes.${palette.name} = palette.replaceVars ../templates/btop.theme;
+    };
 
-  programs.fzf = {
-    enable = true;
-    enableFishIntegration = true;
+    difftastic = {
+      enable = true;
+      git.enable = true;
+    };
+
+    fzf = {
+      enable = true;
+      enableFishIntegration = true;
+    };
   };
 }
--- home/programs/env.nix
+++ home/programs/env.nix
@@ -1,21 +1,22 @@
 { ... }:
 
 {
-  programs.direnv = {
-    enable = true;
-    nix-direnv.enable = true;
-    enableFishIntegration = true;
-    silent = true;
-
-    config = {
-      global = {
-        hide_env_diff = true;
+  programs = {
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+      enableFishIntegration = true;
+      silent = true;
+      config = {
+        global = {
+          hide_env_diff = true;
+        };
       };
     };
-  };
 
-  programs.nix-your-shell = {
-    enable = true;
-    enableFishIntegration = true;
+    nix-your-shell = {
+      enable = true;
+      enableFishIntegration = true;
+    };
   };
 }
