a228bee9d2349f6d23c6401f2a018de852ec2747
Yu <echo.aimless@gmail.com>
2026-09-13 21:00:18 +0800

    chore: tweak gh-dash theme

--- home/programs/gh.nix
+++ home/programs/gh.nix
@@ -1,10 +1,41 @@
-{ pkgs, ... }:
+{ palette, ... }:
 
 {
   programs.gh = {
     enable = true;
-    extensions = [
-      pkgs.gh-dash
-    ];
+  };
+
+  programs.gh-dash = {
+    enable = true;
+
+    settings.theme.colors = {
+      text = {
+        primary = palette.text.primary;
+        secondary = palette.text.muted;
+        inverted = palette.terminal.background;
+        faint = palette.ui.active;
+        warning = palette.status.warning;
+        success = palette.status.success;
+        error = palette.status.error;
+        actor = palette.text.muted;
+      };
+
+      background.selected = palette.ui.hover;
+
+      border = {
+        primary = palette.ui.focusedBorder;
+        secondary = palette.ui.active;
+        faint = palette.ui.border;
+      };
+
+      icon = {
+        newcontributor = palette.text.muted;
+        contributor = palette.terminal.foreground;
+        collaborator = palette.accent.hint;
+        member = palette.status.info;
+        owner = palette.accent.primary;
+        unknownrole = palette.ui.active;
+      };
+    };
   };
 }
