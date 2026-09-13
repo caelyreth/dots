0caff076f5672801350a1179a8c29cd8e9ae4660
Yu <echo.aimless@gmail.com>
2026-09-13 20:11:28 +0800

    chore: setup nix dev shell + fmt

--- flake.nix
+++ flake.nix
@@ -2,9 +2,7 @@
   description = "Yu's macOS configuration";
 
   inputs = {
-    nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     nix-darwin = {
       url = "github:nix-darwin/nix-darwin/master";
@@ -21,9 +19,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    llm-agents = {
-      url = "github:numtide/llm-agents.nix";
-    };
+    llm-agents.url = "github:numtide/llm-agents.nix";
   };
 
   outputs =
@@ -41,33 +37,56 @@
         system = "aarch64-darwin";
         configurationDirectory = "/etc/nix-darwin";
       };
-      pkgs = nixpkgs.legacyPackages.${machine.system};
-      palette = import ./home/palette.nix { inherit pkgs; };
+
+      pkgs = import nixpkgs {
+        system = machine.system;
+      };
+
+      palette = import ./home/palette.nix {
+        inherit pkgs;
+      };
     in
     {
-      homeConfigurations.${machine.username} = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        extraSpecialArgs = {
-          inherit inputs machine palette;
+      homeConfigurations.${machine.username} =
+        home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+
+          extraSpecialArgs = {
+            inherit inputs machine palette;
+          };
+
+          modules = [
+            sops-nix.homeManagerModules.sops
+            ./home
+            {
+              home = {
+                username = machine.username;
+                homeDirectory = "/Users/${machine.username}";
+              };
+            }
+          ];
         };
-        modules = [
-          sops-nix.homeManagerModules.sops
-          ./home
-          {
-            home = {
-              username = machine.username;
-              homeDirectory = "/Users/${machine.username}";
-            };
-          }
+
+      darwinConfigurations.${machine.hostname} =
+        nix-darwin.lib.darwinSystem {
+          specialArgs = {
+            inherit machine;
+          };
+
+          modules = [
+            ./darwin
+          ];
+        };
+
+      devShells.${machine.system}.default = pkgs.mkShellNoCC {
+        packages = [
+          pkgs.nixd
+          pkgs.nixfmt-tree
         ];
+
+        DEV_SHELL_NAME = "nix-darwin";
       };
 
-      darwinConfigurations.${machine.hostname} = nix-darwin.lib.darwinSystem {
-        specialArgs = { inherit machine; };
-
-        modules = [
-          ./darwin
-        ];
-      };
+      formatter.${machine.system} = pkgs.nixfmt-tree;
     };
 }
