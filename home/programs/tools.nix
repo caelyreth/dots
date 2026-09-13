2b16c33a54d4336b092f5b1dab9b075a78a7d250
Yu <echo.aimless@gmail.com>
2026-09-13 12:05:03 +0800

    chore: setup bat with eclat theme

--- home/programs/tools.nix
+++ home/programs/tools.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ palette, pkgs, ... }:
 
 {
   programs.eza = {
@@ -10,5 +10,28 @@
   programs.zoxide = {
     enable = true;
     enableFishIntegration = true;
+  };
+
+  programs.bat = {
+    enable = true;
+    config.theme = palette.name;
+    themes.${palette.name}.src = pkgs.replaceVars ../templates/bat.tmTheme.plist {
+      theme_name = palette.name;
+      background = palette.terminal.background;
+      cursor = palette.accent.cursor;
+      foreground = palette.terminal.foreground;
+      line_highlight = palette.ui.surface;
+      selection = palette.ui.selection;
+      selection_foreground = palette.text.primary;
+      syntax_comment = palette.syntax.comment;
+      syntax_keyword = palette.syntax.keyword;
+      syntax_string = palette.syntax.string;
+      syntax_function = palette.syntax.function;
+      syntax_type = palette.syntax.type;
+      syntax_constant = palette.syntax.constant;
+      syntax_number = palette.syntax.number;
+      syntax_boolean = palette.syntax.boolean;
+      error = palette.status.error;
+    };
   };
 }
